7f1e9ab0bc0ea7f7dc37013d8d4798bab5487017
Blastose <blastoisejay@gmail.com>
2022-06-03 13:40:13 -0700

    Parse location countries and return country code

--- src/models/location-parser.ts
+++ src/models/location-parser.ts
@@ -1,6 +1,22 @@
+import * as countries from "i18n-iso-countries";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
+
 class LocationParser {
   static parseLocation(location: string): string {
-    // Convert country names to codes
+    const locations = location.split(",");
+
+    if (locations.length > 1) {
+      const country = locations[1].trim();
+      if (countries.isValid(country) && country.length === 2) {
+        return location;
+      }
+
+      const code = countries.getAlpha2Code(locations[1].trim(), "EN");
+
+      return `${locations[0].trim()},${code}`;
+    }
+
     return location;
   }
 }
